40711654dff9d13d4a6ad292d6284b1517ebd985
Yeo Jin Yan <jinyan@Yeos-MacBook-Pro.local>
2020-08-14 00:47:52 +0800

    adding image

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY requirements.txt /src/requirements.txt
 RUN pip install -r /src/requirements.txt
 COPY app.py /src
 COPY buzz /src/buzz
+COPY IMG_2664.JPG /src
+COPY index.html /src
 CMD python /src/app.py
--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-
 import os
+import signal
 from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
 
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
 @app.route("/")
 def generate_buzz():
-    buzz = generator.generate_buzz()
-    return render_template('index.html', None)
+    # buzz = generator.generate_buzz()
+    return render_template('index.html')
 
-    return page
+    # return page
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
